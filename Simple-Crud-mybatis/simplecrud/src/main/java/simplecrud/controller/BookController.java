8accc0ff7a0fc5c741555e8770b534bb0baa6f97
clh0127 <811418259@qq.com>
2021-04-17 09:59:02 +0800

    蔡凌涵

--- Simple-Crud-mybatis/simplecrud/src/main/java/simplecrud/controller/BookController.java
+++ Simple-Crud-mybatis/simplecrud/src/main/java/simplecrud/controller/BookController.java
@@ -1,5 +1,7 @@
 package simplecrud.controller;
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import simplecrud.domain.Book;
 import simplecrud.service.BookService;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
+@Controller
 public class BookController {
 
     @Autowired
--- Simple-Crud-mybatis/simplecrud/src/main/java/simplecrud/service/BookService.java
+++ Simple-Crud-mybatis/simplecrud/src/main/java/simplecrud/service/BookService.java
@@ -1,11 +1,13 @@
 package simplecrud.service;
 
+import org.springframework.stereotype.Service;
 import simplecrud.domain.Book;
 import simplecrud.mapper.BookMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
+@Service
 public class BookService {
     @Autowired
     private BookMapper bookMapper;
